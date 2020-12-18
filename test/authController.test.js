c48ab08a8cf9b2255c90539c6f333d9097b06251
mrtrvl <mrt@mrt.ee>
2020-12-18 16:26:10 +0200

    Update tests

--- test/authController.test.js
+++ test/authController.test.js
@@ -11,6 +11,14 @@ const wrongUser = {
   password: 'juku'
 };
 
+const missingEmail = {
+  password: 'juku'
+}
+
+const missingPassword = {
+  email: 'juku@juku.ee'
+}
+
 describe('POST /api/login', function() {
   it('responds with success: true and token', async function() {
     const res = await request(app)
@@ -27,5 +35,21 @@ describe('POST /api/login', function() {
     assert.equal(res.statusCode, 401);
     assert.isFalse(res.body.success);
     assert.ok(res.body.message);
+  });
+  it('responds with success: false and message', async function() {
+    const res = await request(app)
+      .post('/api/login')
+      .send(missingEmail);
+    assert.equal(res.statusCode, 400);
+    assert.isFalse(res.body.success);
+    assert.equal(res.body.message, 'Required field(s) missing or invalid');
+  });
+  it('responds with success: false and message', async function() {
+    const res = await request(app)
+      .post('/api/login')
+      .send(missingPassword);
+    assert.equal(res.statusCode, 400);
+    assert.equal(res.body.message, 'Required field(s) missing or invalid');
+    assert.isFalse(res.body.success);
   });
 });
